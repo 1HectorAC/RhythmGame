287e3b427c5cd34cfa91ab1c3599495f9692ff58
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2020-04-22 14:45:02 -0700

    Cleaned up code and comments in SideBar.cpp file.

--- SideBar.cpp
+++ SideBar.cpp
@@ -1,32 +1,33 @@
+#include <QImage>
 #include "SideBar.h"
 #include "Game.h"
-#include <QImage>
 #include "math.h"
 
 extern Game * game;
-SideBar::SideBar(QGraphicsItem *parent): QObject(), QGraphicsPixmapItem(parent)
-{
-        QImage images("resources/squareObj.png");
-        objHeight = round(game->GLOBAL_HEIGHT / 16);
-        setPixmap(QPixmap::fromImage(images.scaled(objHeight,objHeight)));
 
-        //set the point in the y-axis the object is in
-        positionPoint = 0;
+SideBar::SideBar(QGraphicsItem *parent): QObject(), QGraphicsPixmapItem(parent){
+    //Setup image object.
+    QImage images("resources/squareObj.png");
+    objHeight = round(game->GLOBAL_HEIGHT / 16);
+    setPixmap(QPixmap::fromImage(images.scaled(objHeight,objHeight)));
+
+    //Set postitionPoint to 0. This signifies its position over a limiited y-axis values
+    positionPoint = 0;
 }
 
-void SideBar::moveUp()
-{
+//Move the object up by adjusting its y position value.
+void SideBar::moveUp(){
+    //Moves up only if its position doesn't hit limit.
     if(positionPoint < 6){
         setPos(x(), y() - objHeight);
         positionPoint++;
     }
-
-
 }
 
+//Move the object down by adjusting its y position value.
 void SideBar::moveDown()
 {
-
+    //Moves down only if its position doesn't hit limiit.
     if(positionPoint > -4){
         setPos(x(), y() + objHeight);
         positionPoint--;
